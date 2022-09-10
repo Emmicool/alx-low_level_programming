62d154cc8362729ffc017a0687de2dd695ed4e80
Emmicool <ennamani202@gmail.com>
2022-09-09 17:00:12 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num;
+	char alph;
+
+	num = '0';
+	alph = 'a';
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	while (alpha <= 'f')
+	{
+	putchar(alpha);
+	alpha++;
+	}
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
