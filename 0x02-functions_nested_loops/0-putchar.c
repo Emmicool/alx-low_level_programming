cd499df74f54be4309e034839e08d036a6f2d7b6
Emmicool <ennamani202@gmail.com>
2022-09-13 14:30:29 -0700

    Initial Task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+
+/**
+* main - Description: _putchar
+*
+* Return: Always (0) Success
+*/
+
+int main(void)
+{
+	int _putchar;
+
+	printf("_putchar %d \n");
+
+	return (0);
+}
