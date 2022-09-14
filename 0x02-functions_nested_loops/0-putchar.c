aa49e37a6b623320a89c50c78e50d1cd00e15a2a
Emmicool <ennamani202@gmail.com>
2022-09-14 14:38:47 -0700

    Task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,22 @@
 #include "main.h"
 
 /**
-* main - Description: _putchar
+* main - prints _putchar
 *
 * Return: Always (0) Success
 */
 
 int main(void)
 {
-	int _putchar;
-
-	print_putchar();
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_alphabet - Print Alphabets.
+*
+* Return: Always 0
+*/
+
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+
+	-putchar('\n');
+}
