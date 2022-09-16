100843e7842674ff9b35fdd7bd725fe4f9bf7c38
Emmicool <ennamani202@gmail.com>
2022-09-16 14:44:58 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#inlcude "main.h"
+
+/**
+* mul - check the code.
+* @a: decleared variable.
+* @b: decleared variable.
+* Return: Always 0.
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
