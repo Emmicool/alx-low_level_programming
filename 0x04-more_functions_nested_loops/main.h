9a8a27ad4a1e4a513b86f3ae425d622bf265cc6f
Emmicool <ennamani202@gmail.com>
2022-09-16 14:30:57 -0700

    Initial Task

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+#include <stdio.h>
+
+
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+int _putchar(char);
+void print_triangle(int);
+void print_number(int);
+
+
+
+#endif
